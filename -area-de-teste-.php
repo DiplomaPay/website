b7d1af68ac2c5c54abd17f166d255775af06dee4
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-01-04 16:24:59 -0300

    signup sys

--- -area-de-teste-.php
+++ -area-de-teste-.php
@@ -44,5 +44,56 @@
 	        })
 	    }
 	</script>
+
+
+<!--Login-->
+	
+	<hr>
+	<h1>SignUp área </h1>
+	<form action="javascript:void(0)" onsubmit="sendSignup()">
+	    <input id='email_signup' type="text" placeholder="Email"/>
+	    <input id='password_signup' type="password" placeholder="Password"/>
+	    <input id='confirm_password_signup' type="password" placeholder="Password"/>
+	    <input id='cpf_signup' type="number" placeholder="CPF"/>
+	    <input id='name_signup' type="text" placeholder="Full name"/>
+	    <button>Enviar</button>
+	</form>
+	<p id='res_signup'></p>
+	<hr>
+	
+    <!--Login	-->
+	<script>
+        const email_signup = document.getElementById("email_signup");
+        const password_signup = document.getElementById("password_signup");
+        const confirm_password_signup = document.getElementById("confirm_password_signup");
+        const cpf_signup = document.getElementById("cpf_signup");
+        const name_signup = document.getElementById("name_signup");
+        
+	    const sendSignup = () => {
+	        let email = email_signup.value;
+	        let password = password_signup.value;
+			let confirm_password = confirm_password_signup.value;
+	        let cpf = cpf_signup.value;
+			let name = name_signup.value;
+	        
+	        let data = {
+	            email: email,
+	            password: password,
+				confirm_password: confirm_password,
+				cpf: cpf,
+				name: name
+	        }
+	        
+	        fetch("./sys/user/account/signup", {
+	            method: "POST",
+	            body: JSON.stringify(data),
+	        })
+	        .then(e=>e.json())
+	        .then(e=>{
+	            res_login.innerText = JSON.stringify(e);
+	        })
+	    }
+	</script>
+
 </body>
 </html>
--- sys/user/account/signup.php
+++ sys/user/account/signup.php
@@ -6,18 +6,69 @@ header('Content-Type: application/json; charset=utf-8');
 $request = file_get_contents('php://input');
 $json = json_decode($request);
 
+
+// Recived data 
 $email = $json->email;
 $password = $json->password;
 $confirm_password = $json->confirm_password;
+$cpf = $json->cpf;
+$name = $json->name;
 
-$password_hash = password_hash($password, PASSWORD_DEFAULT);
-$email_verify = filter_var($email, FILTER_VALIDATE_EMAIL);
+// Processed data
+$email = mysqli_real_escape_string($conexao, $email);
+$password = mysqli_real_escape_string($conexao, $password);
+$confirm_password = mysqli_real_escape_string($conexao, $confirm_password);
+$cpf = mysqli_real_escape_string($conexao, $cpf);
+$name = mysqli_real_escape_string($conexao, $name);
 
-if($email and $password == $confirm_password){
-    
+
+// STEP 1 -> Verify data
+if(!$email or !$password or !$confirm_password or !$cpf or !$name){
+    $obj = array(status => $__STATUS__, response => false, message => "Something is missing!");
+    endCode($obj);
 }
 
-$obj = array(status => $__STATUS__, log_status => false, message => "Failed to login", email => $email_verify, password => $password_hash);
 
-echo json_encode($obj);
+// STEP 2 -> Check password
+if($password !== $confirm_password){
+    $obj = array(status => $__STATUS__, response => false, message => "Check password and try again.");
+    endCode($obj);
+}
 
+// hash password 
+$password = password_hash($password, PASSWORD_DEFAULT);
+
+//STEP 3 -> Check user email database
+$queryCheckUserEmail = mysqli_query($conexao, "select * from users where email='$email'") or endCodeError();
+
+if(mysqli_num_rows($queryCheckUserEmail) > 0){
+    $obj = array(status => $__STATUS__, response => false, message => "Email already in use.");
+    endCode($obj);
+}
+
+//STEP 4 -> Check user CPF database
+$queryCheckUserEmail = mysqli_query($conexao, "select * from users where cpf='$cpf'") or endCodeError();
+
+if(mysqli_num_rows($queryCheckUserEmail) > 0){
+    $obj = array(status => $__STATUS__, response => false, message => "CPF already in use.");
+    endCode($obj);
+}
+
+//STEP 5 -> Register user
+$time = time();
+
+mysqli_query($conexao, "insert into users (email, password, cpf, full_name, created) values ('$email', '$password', '$cpf', '$full_name', '$time')") or endCodeError();
+$obj = array(status => $__STATUS__, response => false, message => "Sucess.");
+endCode($obj);
+
+
+
+function endCode($obj){
+    echo json_encode($obj);
+    exit;
+}
+
+function endCodeError(){
+    echo json_encode(array(status => $__STATUS__, response => false, message => "Failed to connect the server, try again."));
+    exit;
+}
