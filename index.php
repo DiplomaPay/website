16547c25856e96a89e3c89efb1e714d785a50e7d
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-01-04 02:03:16 -0300

    organization

--- index.php
+++ index.php
@@ -3,19 +3,19 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./css/styles.css">
-  <link rel="stylesheet" href="./css//global.css">
+  <link rel="stylesheet" href="./items/css/styles.css">
+  <link rel="stylesheet" href="./items/css//global.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet">
   <title>Login</title>
-  <script src="./js/login.js"></script>
+  <script src="./items/js/login.js"></script>
 </head>
 <body>
     <div class="container">
         <article>
-            <img src="./img/Pitch DiplomaPay.svg" alt="DiplomaPay Logo">
+            <img src="./items/img/Pitch DiplomaPay.svg" alt="DiplomaPay Logo">
     
             <h3>LOGIN</h3>
     
