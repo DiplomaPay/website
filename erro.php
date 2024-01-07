3afd1fc378f15e343dc1a8fddd5e6a394b8b6eda
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-01-06 22:00:46 -0300

    Error page

--- erro.php
+++ erro.php
@@ -11,8 +11,36 @@ if(!$status or !in_array($status, $valid_status)){
     http_response_code($status);
 }
 
-echo "
-    <h1>$status</h1>
-    <a href='./'>Voltar</a>
-";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erro <?php echo $status; ?></title>
+    <style>
+        *{
+            font-family: sans-serif;
+            margin: 0;
+            padding: 0;
+            color: white;
+        }
+        body{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            gap: 20px;
+            background: black;
+        }
+        h1{
+            font-size: 60px;
+        }
+    </style>
+</head>
+<body>
+    <h1><?php echo $status ?></h1>
+    <a href="./">Página inicial</a>
+</body>
+</html>
