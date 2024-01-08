4899f1274894cc1edbee0ec65d07a1ce3fe849e0
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-01-07 23:29:05 -0300

    Pix teste 1

--- sys/payment/pix.php
+++ sys/payment/pix.php
@@ -15,14 +15,14 @@ $data = array(
 );
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://api.mercadopago.com/v1/payments/1320525853',
+  CURLOPT_URL => 'https://api.mercadopago.com/v1/payments',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 0,
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_CUSTOMREQUEST => 'POST',
   CURLOPT_POSTFIELDS => json_encode($data),
   CURLOPT_HTTPHEADER => array(
     'Authorization: Bearer TEST-5056284625992718-010720-0ed75a2e6c7caf2596f0d792490b4134-50812775',
