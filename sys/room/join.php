e8880c4b0c3818ac10d27b72e1fd8a0c026d51e9
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-01-10 22:37:20 -0300

    .

---
+++ sys/room/join.php
@@ -0,0 +1,12 @@
+<?php
+include"../conexao.php";
+
+header('Content-Type: application/json; charset=utf-8');
+
+$request = file_get_contents('php://input');
+$json = json_decode($request);
+
+$room_code = $json->room_code;
+
+$room_code = mysqli_real_escape_string($conexao, $room_code);
+
