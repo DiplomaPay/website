011ef3c10169404b64a368d6f092b9a075325319
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-01-16 18:06:14 -0300

    .

--- sys/payment/pix/pay.php
+++ sys/payment/pix/pay.php
@@ -9,11 +9,10 @@ $request = file_get_contents('php://input');
 $json = json_decode($request);
 
 $ammount = $json->value;
-$ammount = int($ammount);
+$ammount = (float) $ammount;
 $ammount = number_format($ammount, 2, '.', '');
 
 $ammount = mysqli_real_escape_string($conexao, $ammount);
-$ammount = int($ammount);
 
 $data = array(
     'transaction_amount' => $ammount,
