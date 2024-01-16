c53cd060c91378ee248aa6e98e86e8362630757e
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-01-16 17:49:08 -0300

    .

--- sys/payment/pix/pay.php
+++ sys/payment/pix/pay.php
@@ -3,12 +3,14 @@ include"../../conexao.php";
 
 header('Content-Type: application/json; charset=utf-8');
 
+$curl = curl_init();
+
 $request = file_get_contents('php://input');
 $json = json_decode($request);
 
 $ammount = $json->value;
-// $ammount = floatval($ammount);
-// $ammount = number_format($ammount, 2);
+$ammount = floatval($ammount);
+$ammount = number_format($ammount, 2);
 
 $ammount = mysqli_real_escape_string($conexao, $ammount);
 
