bc2cf719aa6882ec0cf902e68ae667b6fdee677c
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-01-19 19:41:51 -0300

    teste email

--- sys/user/account/signup.php
+++ sys/user/account/signup.php
@@ -103,16 +103,16 @@ $message = "
 
 $headers[] = 'MIME-Version: 1.0';
 $headers[] = 'Content-type: text/html; charset=iso-8859-1';
-$headers[] = 'From: contato@dpay.trive.fun';
-$headers[] = 'Reply-To: contato@dpay.trive.fun';
+$headers[] = 'From: contatos@dpay.trive.fun';
+$headers[] = 'Reply-To: contatos@dpay.trive.fun';
 $headers[] = 'X-Mailer: PHP/' . phpversion();
 
 $sendEmail = mail($to, $subject, $message, implode("\r\n", $headers));
 
-// if(!$sendEmail) {
+if(!$sendEmail) {
     $obj = array(status => $__STATUS__, email=>$sendEmail ,response => false, message => "Falha ao enviar email, clique para reenviar.");
     endCode($obj);
-// }
+}
 
 $obj = array(status => $__STATUS__, response => true, message => "Sucess.");
 endCode($obj);
