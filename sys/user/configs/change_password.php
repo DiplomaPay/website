04680aecae87576f06e15bca2d3eb8ec3354ca2c
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-01-19 20:49:50 -0300

    .

--- sys/user/configs/change_password.php
+++ sys/user/configs/change_password.php
@@ -110,7 +110,13 @@ function sendMail($conexao, $email){
     </html>
     ";
 
-    $sendEmail = mail($to, $subject, $message, implode("\r\n", $__HEADERS__));
+    $headers[] = 'MIME-Version: 1.0';
+    $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+    $headers[] = 'From: contatoasdsad@dpay.trive.fun';
+    $headers[] = 'Reply-To: contato@dpay.trive.fun';
+    $headers[] = 'X-Mailer: PHP/' . phpversion();
+
+    $sendEmail = mail($to, $subject, $message, implode("\r\n", $headers));
 
     if(!$sendEmail) {
         $obj = array(status => $__STATUS__, response => false, message => "Falha ao enviar email, tente novamente.");
