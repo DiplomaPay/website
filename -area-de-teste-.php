f39fd54c9d60939f2f56f31af206077857923a9a
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-01-20 16:58:22 -0300

    .

--- -area-de-teste-.php
+++ -area-de-teste-.php
@@ -306,7 +306,7 @@
 				console.log(data)
 	            for(let i = 0; i < data.length; i++){
 					myrooms_list.innerHTML += `
-						<div>id: ${data[i].id} - Name: ${data[i].room_name} - Code: ${data[i].room_code} - Valor na sala: ${data[i].ammount_room}- Usuário: ${data[i].typeuser} <button onclick='newPayment("${data[i].room_code}")'>Novo pagamento R$0.01</button></div>
+						<div>id: ${data[i].id} - Name: ${data[i].room_name} - Code: ${data[i].room_code} - Valor na sala: R$${data[i].ammount_room} - Usuário: ${data[i].typeuser} <button onclick='newPayment("${data[i].room_code}")'>Novo pagamento R$0.01</button></div>
 					`;
 				}
 	        })
