7c2d2df87d5124c6c721d198751fb79e315e8a7d
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-01-20 02:59:48 -0300

    .

---
+++ sys/payment/refund.php
@@ -0,0 +1,58 @@
+<?php
+include"../conexao.php";
+
+justLog($__EMAIL__);
+
+header('Content-Type: application/json; charset=utf-8');
+
+$pay_id = $_GET["id"];
+
+$queryPayment = mysqli_query($conexao, "select * from payment_pix where pay_id='$pay_id'")or endCodeError();
+
+if(mysqli_num_rows($queryPayment) < 1){
+    $obj = array(status => $__STATUS__, response => false, message => "Nenhum pagamento com esse id");
+    endCode($obj);
+}
+
+$d = mysqli_fetch_assoc($queryPayment);
+
+$statusPayment = $d['status'];
+$ammountPayment = $d['ammount'];
+
+if($statusPayment != "approved"){
+    $obj = array(status => $__STATUS__, response => false, message => "Não elegivel para reembolso");
+    endCode($obj);
+}
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.mercadopago.com/v1/payments/$pay_id/refunds",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_HTTPHEADER => array(
+    'Authorization: Bearer APP_USR-5056284625992718-010720-8f3ef5796104ab4ed6dd46bda8cc493c-50812775',
+    'Public-Key: APP_USR-7fb9bd3b-0834-4195-8c52-2c22ba74253c',
+    'Content-Type: application/json'
+  ),
+  CURLOPT_POSTFIELDS => json_encode(array(
+    'amount' => $ammountPayment
+  ))
+));
+
+$response = curl_exec($curl);
+$res = json_decode($response);
+curl_close($curl);
+
+endCode($res);
+
+function endCode($obj){
+    echo json_encode($obj);
+    exit;
+}
+
+function endCodeError(){
+    echo json_encode(array(status => $__STATUS__, response => false, message => "Failed to connect the server, try again."));
+    exit;
+}
+
