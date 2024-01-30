9b8dbe14770360541d523d3617ec2b8a6a75f91a
Ximbeh <senhorximbinha@gmail.com>
2024-01-30 18:19:11 -0300

    css de dashboard

--- p/dashboard.php
+++ p/dashboard.php
@@ -1,21 +1,25 @@
 
 <?php
-// include"../sys/conexao.php";
+include"../sys/conexao.php";
 //só consegue entrar logado
-// justLog($__EMAIL__);
+justLog($__EMAIL__);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../items/css/dashboard.css">
+    <link rel="stylesheet" href="../items/css/dash.css">
   <link rel="stylesheet" href="../items/css/global.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet">
     <title>Dash</title>
 </head>
 <body>
     <section class="sidebar">
-        <img src="../items/img/IconBranco.svg" alt="DiplomaPay Logo">
+        <img src="../items/img/IconBranco.svg" alt="DiplomaPay Logo" class = "logo">
         <div>
             <div>
                 <img src="" alt="">
@@ -42,26 +46,26 @@
     <section class="main">
         <header>
             <div>
-                <h2>Bem vindo(a), </h2>
-                <h2>Fulano Ciclano!</h2>
+                <h2>Bem vindo(a), <span>Fulano Ciclano</span>!</h2>
             </div>
-            <img src="" alt="" class="userImage">
+            <img src="../items/img/profile.jpeg" alt="" class="userImage">
         </header>
 
         <div class="legend">
                 <p>Dados da sala</p>
                 <p>Seus dados</p>
         </div>
-        <div>
-            <h3>Vocé é um contribuinte nota: B</h3>
+        <div class="notaContribuinte">
+            <h3>Vocé é um contribuinte nota:</h3> 
+            <span class="nota">B</span>
             <br>
-            <h4>(Coloque R$8,94 e vire contribuinte nota A. Sua parte ficaria em R$544,68)</h4>
+            <h4>(Coloque <span>R$8,94</span> e vire contribuinte nota <span>A</span>. Sua parte ficaria em <span>R$544,68</span>)</h4>
         </div>
 
         <div class="enquete">
             <div>
                 <h3>Tem uma enquete acontecendo!</h3>
-                <img src="" alt="" class="estrelinha">
+                <img src="../items/img/estrelinha.svg" alt="" class="estrelinha">
             </div>
             <div>
                 <p>Devemos gastar a grana no cassino?</p>
@@ -86,11 +90,11 @@
                     <h4>Total guardado</h4>
                     <p>R$ 12.244,96</p>
                 </div>
-                <div>
+                <div class="pessoal">
                     <h4>Sua parte</h4>
                     <p>R$ 385,83</p>
                 </div>
-                <div>
+                <div class="pessoal">
                     <h4>Suas constribuições</h4>
                     <p>R$ 129,33</p>
                 </div>
@@ -102,14 +106,14 @@
                     <h4>Total de clientes</h4>
                     <p>6</p>
                 </div>
-                <div>
+                <div class="pessoal">
                     <h4>Contribuição média</h4>
                     <p>R$ 138,27</p>
                 </div>
             </div>
-            <div>
+            <div class="graphic">
                 <h3>Titulo</h3>
-                <div class="graphic">
+                <div class="graphicImage">
 
                 </div>
             </div>
