b1c45ba4f3b1a28813128374b892359c71a102b5
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-02-15 16:31:41 -0300

    .

--- p/room.php
+++ p/room.php
@@ -200,6 +200,7 @@
                                 <p>R$${data[i].ammount_room}</p><p>Saldo</p>
                             </div>
                             <button>Entrar no painel</button>
+                            <button onclick="copy('${data[i].room_code}')" style="background: none; color: black; font-size: 1.1vw">Copiar código da sala</button>
                         </div>
                     `;
                 }
