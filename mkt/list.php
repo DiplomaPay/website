22e9c7931b1894fd1e1765179a641202d8e9e367
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-05-20 01:41:54 -0300

    .

--- mkt/list.php
+++ mkt/list.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de produtos</title>
+    <script src="js/global.js"></script>
     <script src="js/cart.js"></script>
 </head>
 <body>
