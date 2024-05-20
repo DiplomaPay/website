4727d4e8fe3f9eb53f15fe37c7fa79838c84f5c3
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-05-20 01:23:16 -0300

    .

--- mkt/js/cart.js
+++ mkt/js/cart.js
@@ -14,25 +14,22 @@ function updateCart(){
 // id quant 
 function updateItem(id, type, limit){
     let index = cart.findIndex(obj => obj.id === id);
-    console.log(index);
-    return;
 
-    let val = res.quant
     if(type == 0){
         if(val < 2){
-            delete cart[`i${id}`];
+            delete cart[index];
         } else {
             if(val < limit){
-                Object.assign(cart.find(obj => obj.id === id), {quant: val - 1})
+                cart[index].quant += 1;
             }
         }
     } else {
-        Object.assign(cart.find(obj => obj.id === id), {quant: val + 1})
+        cart[index].quant += 1;
     }
     updateCart();
     setCart();
 }
 
 function sendCart(){
-    
+    console.log(cart);
 }
