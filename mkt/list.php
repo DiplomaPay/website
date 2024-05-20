4822cc4eb94107f28dfc860bfae96e45632c6997
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-05-20 01:24:22 -0300

    .cd

--- mkt/list.php
+++ mkt/list.php
@@ -1,13 +1,16 @@
-<!-- listar itens (user) -> botao deletar e editar (adm) -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de produtos</title>
+    <script src="js/cart.js"></script>
 </head>
 <body>
     <div id='productsList'>
+        <button onclick="updateItem(1, 0, 10)">Remove</button>
+        <button onclick="addCart(1, 1, 10)">Add</button>
+        <button onclick="sendCart()">Send</button>
     </div>
 
     <script>
