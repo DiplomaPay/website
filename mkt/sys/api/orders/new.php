cf1ec00ba25122002c395c5ef32a77befe93639f
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-05-19 22:47:57 -0300

    .

--- mkt/sys/api/orders/new.php
+++ mkt/sys/api/orders/new.php
@@ -8,7 +8,7 @@ $json = json_decode($request);
 
 $total = 0;
 
-$products  = scapeString($__CONEXAO__, $json->items);
+$products  = $json->items;
 foreach($products as $item){
     $item = setArray($item, 'setNum');
     $item = setArray($item, 'decrypt');
@@ -33,7 +33,6 @@ foreach($products as $item){
     
     $total += $price * $qt;
 }
-endCode($products, false);
 
 $total = setNoXss(($total));
 
