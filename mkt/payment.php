fc9428f5d3848e786f912ebd43996fc3726fa21d
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-05-21 16:07:47 -0300

    .

--- mkt/payment.php
+++ mkt/payment.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagamento</title>
-    <script src="./js/global.js"></script>
-    <script src="./js/cart.js"></script>
+    
 </head>
 <body>
     Lista de itens
@@ -21,6 +20,8 @@
     <p id='payprice'></p>
     <p id='paycode'></p>
 
+    <script src="./js/global.js"></script>
+    <script src="./js/cart.js"></script>
     <script>
         for(let i of cart){
             lista.innerHTML += `
