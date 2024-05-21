e619e736976ad4e01aad7a031811576beef2125a
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-05-21 16:06:15 -0300

    .

--- mkt/payment.php
+++ mkt/payment.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagamento</title>
-    <script src="js/global.js"></script>
-    <script src="js/cart.js"></script>
+    <script src="./js/global.js"></script>
+    <script src="./js/cart.js"></script>
 </head>
 <body>
     Lista de itens
